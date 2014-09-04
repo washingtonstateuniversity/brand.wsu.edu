6f0345c9f49523692e55a859bb3df4f494d3e942
Nathan Jacobson <nate.jacobson@wsu.edu>
2014-09-03 17:27:56 -0700

    Add scrolly parallax

--- scripts/brand.js
+++ scripts/brand.js
@@ -11,13 +11,121 @@ $( document ).ready(function() {
 
 });
 
+(function ( $, window, document, undefined ) {
+    // Create the defaults once
+    var pluginName = 'scrolly',
+        defaults = {
+            bgParallax: false
+        },
+        didScroll = false;
+
+    function Plugin( element, options ) {
+        this.element = element;
+        this.$element = $(this.element);
+
+        this.options = $.extend( {}, defaults, options) ;
+        
+        this._defaults = defaults;
+        this._name = pluginName;
+        
+        this.init();
+    }
+
+    Plugin.prototype.init = function () {
+        var self = this;
+        this.startPosition = this.$element.position().top;
+        this.offsetTop = this.$element.offset().top;
+        this.height = this.$element.outerHeight(true);
+        this.velocity = this.$element.attr('data-velocity');
+        this.bgStart = parseInt(this.$element.attr('data-fit'), 10);
+
+        $(document).scroll(function(){
+            self.didScroll = true;
+        });
+        
+        setInterval(function() {
+            if (self.didScroll) {
+                self.didScroll = false;
+                self.scrolly();
+            }
+        }, 10);
+    };
+
+    Plugin.prototype.scrolly = function() {
+        var dT =  $(window).scrollTop(),
+            wH = $(window).height(),
+            position = this.startPosition;
+
+        if(this.offsetTop >= (dT+wH)) {
+            this.$element.addClass('scrolly-invisible');
+        } else {
+            if(this.$element.hasClass('scrolly-invisible')){
+                position = this.startPosition + (dT + ( wH - this.offsetTop ) ) * this.velocity;
+            } else {
+                position = this.startPosition + dT  * this.velocity;
+            }
+        }
+        // Fix background position
+        if(this.bgStart){ position = position + this.bgStart; }
+
+        if(this.options.bgParallax === true) {
+            this.$element.css({backgroundPosition: '50% '+position+'px'});
+        } else {
+            this.$element.css({top: position});
+        }
+    };
+
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+            }
+        });
+    };
+
+})(jQuery, window, document);
+
+
+/*$(window).scroll(function(){
+	var scrollTop = $(window).scrollTop();
+	var windowHeight = $(window).height();		
+	var topmost = false;
+	$("main section figure").each( function() {
+		var offset = $(this).offset();
+		if ( $("body").hasClass("logged-in") ) {
+			scrollTop = scrollTop - 32;
+			}
+		var difference = offset.top - scrollTop;
+		$(this).prepend("&nbsp;o"+offset.top).prepend("&nbsp;s"+scrollTop).prepend("&nbsp;d"+difference);
+		if (scrollTop <= offset.top && ($(this).height() + offset.top) < (scrollTop + windowHeight) && topmost == false) {
+			$(this).addClass("topmost");
+			topmost=true;
+		} else {
+			$(this).removeClass("topmost");
+			topmost=false;
+		}
+		if ( difference < windowHeight ) {
+			$(this).css('background-position-y',-difference/2);
+			
+		}
+	});
+});
+*/
+
+/*
 $(window).scroll(function(){
 	var scrollTop = $(window).scrollTop();
 	var windowHeight = $(window).height();		
 	var topmost = false;
-	$("main section").each( function() {
+	$("main section figure").each( function() {
 		var offset = $(this).offset();
+		var difference = offset.top - scrollTop - 32;
 		if (scrollTop <= offset.top && ($(this).height() + offset.top) < (scrollTop + windowHeight) && topmost == false) {
+			if ( difference < 0 ) {
+			$(this).css('background-position-y',difference);
+			}
+			$(this).prepend("&nbsp;o"+offset.top).prepend("&nbsp;s"+scrollTop).prepend("&nbsp;d"+difference);
+			
 			$(this).addClass("topmost");
 			topmost=true;
 		} else {
@@ -27,7 +135,7 @@ $(window).scroll(function(){
 	});
 });
 
-/*
+
 $( document ).on("ready", "resize" (function() {
 $('.size-gt-medium .main-header, .size-gt-medium .brand-header').hcSticky({
 	    top: 0,
